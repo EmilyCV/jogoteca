478ac1865876592721aa49638ce842ce8777a37b
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-03-28 16:14:07 -0300

    code refactoring and styling

--- jogoteca.py
+++ jogoteca.py
@@ -1,17 +1,41 @@
-from flask import Flask, render_template
-from App.model.jogo import Jogo
+from re import L
+from flask import Flask, redirect, render_template, request, redirect
+
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-# trecho da app
-app.run(host='0.0.0.0', port=8080)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)
