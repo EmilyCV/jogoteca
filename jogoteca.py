490ccba6d83b1189604321db20aa337b575d11be
EmilyCV <emily_cvasconcelos@hotmail.com>
2022-03-28 12:01:55 -0300

    listing games using flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from App.model.jogo import Jogo
 
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
 
 # trecho da app
 app.run(host='0.0.0.0', port=8080)
